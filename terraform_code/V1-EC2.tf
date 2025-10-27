9c16ae5b3d8d03a9d2488d51b505888f20e0cd46
indu <indukumari@gmail.com>
2025-10-27 07:51:30 +0530

    commiting V1-EC2.tf

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,10 @@
+# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
-    instance_type = "t2.micro"
-    key_name = "dpp"
+  ami = "ami-02d26659fd82cf299"
+  instance_type = "t3.micro"
+  key_name = "dpp"
 }
--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -1,12 +1,13 @@
+# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
-    instance_type = "t2.micro"
-    key_name = "dpp"
-    security_groups = [ "demo-sg" ]
+  ami = "ami-02d26659fd82cf299"
+  instance_type = "t3.micro"
+  key_name = "dpp"
+  security_groups = [ "demo-sg"]
 }
 
 resource "aws_security_group" "demo-sg" {
@@ -30,7 +31,7 @@ resource "aws_security_group" "demo-sg" {
   }
 
   tags = {
-    Name = "ssh-prot"
+    Name = "ssh-port"
 
   }
 }
